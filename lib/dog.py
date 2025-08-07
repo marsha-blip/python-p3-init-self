a0d92784c21384ce35097f2356fe8aa5fe9cee9b
Mary <marshareeitumo@gmail.com>
2025-08-07 07:41:09 +0300

    adds lib

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed="Mutt"):
+        """
+        Initialize a new Dog instance.
+
+        :param name: The dog's name.
+        :param breed: The dog's breed; defaults to "Mutt" if not provided.
+        """
+        self.name = name
+        self.breed = breed
--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
+# lib/person.py
+
 class Person:
-    pass
+    def __init__(self, name):
+        """
+        Initialize a new Person instance.
+
+        :param name: The person's name.
+        """
+        self.name = name
+
